ec82f037ba6f34ce228ae82b2e0ec7af9d951473
Никита Забелин <asdfghjkliop44@gmail.com>
2022-02-24 20:23:33 +0300

    Добавьте файлы проекта.

---
+++ OSILabs_4_sem/main.cpp
@@ -0,0 +1,15 @@
+﻿#include <cstdio>
+#include <unistd.h>
+#include <fcntl.h>
+#include <linux/kd.h>
+#include <sys/ioctl.h>
+
+
+
+int main()
+{
+    int fd;
+    fd = open("/dev/console", O_RDWR);
+    ioctl(fd, KDSETLED, 7);
+    return 0;
+}
